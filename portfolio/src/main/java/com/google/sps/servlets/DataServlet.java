0da151c073dfd76919d39fedbdd917bc62186db3
Lily Zhou <lilyzhou@stanford.edu>
2020-06-29 20:51:34 +0000

    Modify doGet() so that it returns an ArrayList of placeholder values as a JSON string.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,6 +15,7 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
@@ -37,8 +38,15 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    //TODO: modify so that doGet returns messages as a JSON string.
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello, Lily!</h1>");
+    String json = convertToJson(messages);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  private String convertToJson(List<String> messageList) {
+    Gson gson = new Gson();
+    String json = gson.toJson(messageList);
+    return json;
   }
 }
