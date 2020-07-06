cb678cfc61c1fe142a50c5e315bc61876b753f0f
Lily Zhou <lilyzhou@stanford.edu>
2020-07-06 23:28:03 +0000

    Fetch data from /chart-data and display on geochart.

--- portfolio/src/main/webapp/charts-script.js
+++ portfolio/src/main/webapp/charts-script.js
@@ -6,19 +6,19 @@ google.charts.setOnLoadCallback(drawChart);
  
 /** Creates a chart and adds it to the page. */
 function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Country', 'Popularity'],
-    ['Germany', 200],
-    ['United States', 300],
-    ['Brazil', 400],
-    ['Canada', 500],
-    ['France', 600],
-    ['RU', 700]
-  ]);
- 
+  fetch('/chart-data').then(response => response.json())
+  .then((countryMap) => {
+    const data = new google.visualization.DataTable();
+    data.addColumn('string', 'Country');
+    data.addColumn('number', 'Rice Production');
+    Object.keys(countryMap).forEach((country) => {
+      data.addRow([country, countryMap[country]]);
+    });
+
   const options = {};
  
   const chart = new google.visualization.GeoChart(
       document.getElementById('chart-container'));
   chart.draw(data, options);
+  });
 }
