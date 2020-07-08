c7286ca813f5fbbec51d955d28dac1036c61c68d
Lily Zhou <lilyzhou@stanford.edu>
2020-07-08 23:28:18 +0000

    Initial attempt at dynamically adding script tag to head.

--- portfolio/src/main/webapp/map-script.js
+++ portfolio/src/main/webapp/map-script.js
@@ -2,8 +2,15 @@ const MAP_ID = 'map-container';
 
 /** Creates a map and adds it to the page. */
 function createMap() {
+  //addScriptToHead();
   const map = new google.maps.Map(
       document.getElementById(MAP_ID),
       // Centered at Googleplex (Mountain View).
       {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
+function addScriptToHead() {
+  var newScript = document.createElement('script');
+  newScript.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyARIbGzg1ObeVicxm6Txr1mqhUR4w12LQQ';
+  document.getElementsByTagName('head')[0].appendChild(newScript);
 }
