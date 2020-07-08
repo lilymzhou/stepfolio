78ef26dd3280eecb5fc291131421db22270a0265
Lily Zhou <lilyzhou@stanford.edu>
2020-07-08 19:18:23 +0000

    Add default map to new page.

---
+++ portfolio/src/main/webapp/map-script.js
@@ -0,0 +1,8 @@
+const MAP_ID = 'map-container';
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById(MAP_ID),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
