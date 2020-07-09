5763b2919291e1b9a9dee0c164090d537b636271
Lily Zhou <lilyzhou@stanford.edu>
2020-07-09 17:07:13 +0000

    Move country name info to new csv file.

--- portfolio/src/main/java/com/google/sps/servlets/ChartServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ChartServlet.java
@@ -28,12 +28,15 @@ public class ChartServlet extends HttpServlet {
 
   private static final String JSON_TYPE = "application/json";
   private static final String DATA_FILE = "/WEB-INF/edible_food_2011.csv";
+  private static final String COUNTRY_FILE = "/WEB-INF/country_name_map.csv";
+  private static final int CSV_INDEX = 0;
+  private static final int GEO_INDEX = 1;
   private static final String UNDEFINED_DATA = "*";
   private static final String FILE_DELIM = ",";
   private static final int COUNTRY_INDEX = 1;
   private static final int RICE_INDEX = 4;
 
-  // Stores data in the form "country_name: rice_supplies."
+  // Stores data in the form "geochart_country_name: rice_supplies."
   private LinkedHashMap<String, Double> countryMap = new LinkedHashMap<>();
 
   // Stores data in the form "csv_country_name : geochart_country_name."
@@ -60,29 +63,21 @@ public class ChartServlet extends HttpServlet {
 
       Double riceConsump = riceStr.equals(UNDEFINED_DATA) ? 0.0 : Double.valueOf(riceStr);
 
-      if (countryNameMap.containsKey(country)) {
-        countryMap.put(countryNameMap.get(country), riceConsump);
-      } else {
-        countryMap.put(country, riceConsump);
-      }
+      String countryName = countryNameMap.containsKey(country) ? countryNameMap.get(country) : country;
+      countryMap.put(countryName, riceConsump);
     }
     scanner.close();
   }
 
   private void fillCountryNameMap() {
-    countryNameMap.put("Russian Federation", "Russia");
-    countryNameMap.put("United States of America", "United States");
-    countryNameMap.put("Viet Nam", "Vietnam");
-    countryNameMap.put("Lao People's Democratic Republic", "Laos");
-    countryNameMap.put("Iran (Islamic Republic of)", "Iran");
-    countryNameMap.put("Sudan (former)", "Sudan");
-    countryNameMap.put("Congo", "DRC");
-    countryNameMap.put("United Republic of Tanzania", "Tanzania");
-    countryNameMap.put("Democratic People's Republic of Korea", "North Korea");
-    countryNameMap.put("Republic of Korea", "South Korea");
-    countryNameMap.put("Venezuela (Bolivarian Republic of)", "Venezuela");
-    countryNameMap.put("Bolivia (Plurinational State of)", "Bolivia");
-    countryNameMap.put("Syrian Arab Republic", "Syria");
+    Scanner countryScanner = new Scanner(getServletContext().getResourceAsStream(COUNTRY_FILE));
+
+    while (countryScanner.hasNextLine()) {
+      String line = countryScanner.nextLine();
+      String[] cells = line.split(FILE_DELIM);
+      countryNameMap.put(cells[CSV_INDEX], cells[GEO_INDEX]);
+    }
+    countryScanner.close();
   }
 
   @Override
