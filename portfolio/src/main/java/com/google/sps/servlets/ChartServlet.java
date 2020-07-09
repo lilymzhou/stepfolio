22fb3fd2f7d23fcb1817cf0fd1004e43e1e8a1b2
Lily Zhou <lilyzhou@stanford.edu>
2020-07-09 00:05:05 +0000

    Modify servlet to adjust country names read from .csv file.

--- portfolio/src/main/java/com/google/sps/servlets/ChartServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ChartServlet.java
@@ -33,17 +33,21 @@ public class ChartServlet extends HttpServlet {
   private static final int COUNTRY_INDEX = 1;
   private static final int RICE_INDEX = 4;
 
-  // Stores data in the form "country_name: rice_produced."
+  // Stores data in the form "country_name: rice_supplies."
   private LinkedHashMap<String, Double> countryMap = new LinkedHashMap<>();
+
+  // Stores data in the form "csv_country_name : geochart_country_name."
+  private LinkedHashMap<String, String> countryNameMap = new LinkedHashMap<>();
 
   @Override
   public void init() {
+    fillCountryNameMap();
+
     Scanner scanner = new Scanner(getServletContext().getResourceAsStream(DATA_FILE));
 
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine();
       String[] cells = line.split(FILE_DELIM);
-
       if (cells.length < RICE_INDEX + 1) {
         continue;
       } 
@@ -56,9 +60,29 @@ public class ChartServlet extends HttpServlet {
 
       Double riceConsump = riceStr.equals(UNDEFINED_DATA) ? 0.0 : Double.valueOf(riceStr);
 
-      countryMap.put(country, riceConsump);
+      if (countryNameMap.containsKey(country)) {
+        countryMap.put(countryNameMap.get(country), riceConsump);
+      } else {
+        countryMap.put(country, riceConsump);
+      }
     }
     scanner.close();
+  }
+
+  private void fillCountryNameMap() {
+    countryNameMap.put("Russian Federation", "Russia");
+    countryNameMap.put("United States of America", "United States");
+    countryNameMap.put("Viet Nam", "Vietnam");
+    countryNameMap.put("Lao People's Democratic Republic", "Laos");
+    countryNameMap.put("Iran (Islamic Republic of)", "Iran");
+    countryNameMap.put("Sudan (former)", "Sudan");
+    countryNameMap.put("Congo", "DRC");
+    countryNameMap.put("United Republic of Tanzania", "Tanzania");
+    countryNameMap.put("Democratic People's Republic of Korea", "North Korea");
+    countryNameMap.put("Republic of Korea", "South Korea");
+    countryNameMap.put("Venezuela (Bolivarian Republic of)", "Venezuela");
+    countryNameMap.put("Bolivia (Plurinational State of)", "Bolivia");
+    countryNameMap.put("Syrian Arab Republic", "Syria");
   }
 
   @Override
