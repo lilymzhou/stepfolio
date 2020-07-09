904815b47cf8239537f426fc91164107078a14cf
Lily Zhou <lilyzhou@stanford.edu>
2020-07-09 23:04:27 +0000

    Read bigfoot sighting data from csv file and display as markers on map.

--- portfolio/src/main/webapp/map-script.js
+++ portfolio/src/main/webapp/map-script.js
@@ -1,14 +1,25 @@
 const MAP_ID = 'map-container';
 const mapKey = config.MAP_KEY;
+const BIGFOOT_URL = '/bigfoot-data';
 
 window.onload = addScriptToHead();
 
 /** Creates a map and adds it to the page. */
 function createMap() {
-  const map = new google.maps.Map(
-      document.getElementById(MAP_ID),
-      // Centered at Googleplex (Mountain View).
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+  fetch(BIGFOOT_URL).then(response => response.json()).then((bigfootArr) => {
+    const map = new google.maps.Map(
+        document.getElementById(MAP_ID),
+        // Centered at Googleplex (Mountain View).
+        {center: {lat: 37.422, lng: -122.084}, zoom: 3});
+
+    bigfootArr.forEach((sighting) => {
+      new google.maps.Marker({
+        position: {lat: sighting.lat, lng: sighting.lng},
+        map: map,
+        title: sighting.title
+      });
+    })
+  });
 }
 
 function addScriptToHead() {
